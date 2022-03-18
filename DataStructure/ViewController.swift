21da3991f8ba525e17bf4c7b4eac80f7b79cc1bd
Luke <wuchiwei17@gmail.com>
2022-03-18 15:12:56 +0800

    Initial Commit

---
+++ DataStructure/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DataStructure
+//
+//  Created by WU CHIH WEI on 2022/3/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
