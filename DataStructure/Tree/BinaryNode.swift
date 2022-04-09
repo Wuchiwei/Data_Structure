938e40d0b0f861cee2ac76591c11aee776f3c88e
Luke <wuchiwei17@gmail.com>
2022-04-09 21:31:11 +0800

    [Update] Add BinaryTree Node 以及三種不同的 Traversal Algorithm (In-order, Pre-order, Post-order)

---
+++ DataStructure/Tree/BinaryNode.swift
@@ -0,0 +1,86 @@
+//
+//  BinaryNode.swift
+//  DataStructure
+//
+//  Created by WU CHIH WEI on 2022/4/8.
+//
+
+import Foundation
+import UIKit
+
+public protocol BinaryNode: CustomStringConvertible {
+    associatedtype Element
+    associatedtype ChildNode: BinaryNode where ChildNode.Element == Element
+    
+    var value: Element { get set }
+    var leftChild: ChildNode? { get set }
+    var rightChild: ChildNode? { get set }
+}
+
+extension BinaryNode {
+
+  public var description: String {
+    diagram(for: self as? ChildNode)
+  }
+
+  private func diagram(for node: ChildNode?,
+                       _ top: String = "",
+                       _ root: String = "",
+                       _ bottom: String = "") -> String {
+    guard let node = node else {
+      return root + "nil\n"
+    }
+    if node.leftChild == nil && node.rightChild == nil {
+      return root + "\(node.value)\n"
+    }
+    return diagram(for: node.rightChild as? ChildNode,
+                   top + " ", top + "┌──", top + "│ ")
+         + root + "\(node.value)\n"
+         + diagram(for: node.leftChild as? ChildNode,
+                   bottom + "│ ", bottom + "└──", bottom + " ")
+  }
+}
+
+final public class LKBinaryNode<E>: BinaryNode {
+    
+    public typealias Element = E
+    
+    public typealias ChildNode = LKBinaryNode<E>
+    
+    public var value: Element
+    
+    public var leftChild: ChildNode?
+    
+    public var rightChild: ChildNode?
+    
+    public init(value: Element) {
+        self.value = value
+    }
+}
+
+//MARK: - In-Order Traversal
+extension LKBinaryNode {
+    public func inOrderTraversal(visit: (Element) -> Void) {
+        leftChild?.inOrderTraversal(visit: visit)
+        visit(value)
+        rightChild?.inOrderTraversal(visit: visit)
+    }
+}
+
+//MARK: - Pre-Order Traversal
+extension LKBinaryNode {
+    public func preOrderTraversal(visit: (Element) -> Void) {
+        visit(value)
+        leftChild?.preOrderTraversal(visit: visit)
+        rightChild?.preOrderTraversal(visit: visit)
+    }
+}
+
+//MARK: - Post-Order Traversal
+extension LKBinaryNode {
+    public func postOrderTraversal(visit: (Element) -> Void) {
+        leftChild?.postOrderTraversal(visit: visit)
+        rightChild?.postOrderTraversal(visit: visit)
+        visit(value)
+    }
+}
